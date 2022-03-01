49be6a8bd6ed427463fabe9ac747486ca948b820
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-01 13:37:50 +0100

    Altered formatting

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Hello from Totally Radical Weather App
-      <br />
-      <a
-        href="https://github.com/meageliz/totally-radical-weather-app"
-        target="_blank"
-        rel="no-referrer"
-      >
-        Open-source code
-      </a>{" "}
-      created by Meag Gardner
+      <h1>Totally Radical Weather App</h1>
+      <p>
+        <a
+          href="https://github.com/meageliz/totally-radical-weather-app"
+          target="_blank"
+          rel="no-referrer"
+        >
+          Open-source code
+        </a>{" "}
+        created by Meag Gardner
+      </p>
     </div>
   );
 }
