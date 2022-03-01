b1702d1ac8b6ed5f82c1cacb6c03b1b3ad23d7ed
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-01 14:14:51 +0100

    Added temp

--- src/Weather.js
+++ src/Weather.js
@@ -15,6 +15,7 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
             alt="sunny"
           />
+          6°C
         </div>
       </div>
       Hello from Weather
