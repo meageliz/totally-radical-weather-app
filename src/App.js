431c25b173b62655a982add31aa6692a2ac9d2de
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-01 13:34:11 +0100

    Added coder cred

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello from Totally Radical Weather App</div>;
+  return (
+    <div className="App">
+      Hello from Totally Radical Weather App
+      <br />
+      <a
+        href="https://github.com/meageliz/totally-radical-weather-app"
+        target="_blank"
+        rel="no-referrer"
+      >
+        Open-source code
+      </a>{" "}
+      created by Meag Gardner
+    </div>
+  );
 }
 
 export default App;
