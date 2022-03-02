369ca21c948ec99439a027bf9b6dad038b79e24b
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-02 17:19:06 +0100

    Took out h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Totally Radical Weather App</h1>
         <Weather />
         <CoderCred />
       </div>
