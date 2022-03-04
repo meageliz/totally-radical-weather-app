d49d585428615605fb463933e7476080012cab7c
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-04 11:47:20 +0100

    Moved content over to WI

--- src/Weather.js
+++ src/Weather.js
@@ -39,34 +39,6 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo />
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <div className="temperature">{weatherData.temperature}</div>
-                <div className="unit">°C</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
-          </ul>
-        </div>
       </div>
     );
   } else {
