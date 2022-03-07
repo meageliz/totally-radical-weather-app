f955008d150cf7a4f0ae16a5796f4b672275b04b
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-07 10:41:24 +0100

    Debugging icon placement

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
@@ -18,7 +19,6 @@ export default function WeatherInfo(props) {
             <span className="float-left">
               <img src={props.data.iconUrl} alt={props.data.description} />
             </span>
-
             <span className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </span>
