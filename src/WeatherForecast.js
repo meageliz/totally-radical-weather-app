973188d722f8b7c9ad54f5bbacc241259e11a5b8
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-07 12:02:25 +0100

    Imported city coords from API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,13 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
-    let apiKey = "53315a4c01471ff10f1bbba4b3a95f94";
-    let longitude = 40.7;
-    let latitiude = 74;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid={apiKey}`;
-`;
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "53315a4c01471ff10f1bbba4b3a95f94";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
