e7888c15d95c17b95d9be81720b925230e5dbcae
meageliz <92804633+meageliz@users.noreply.github.com>
2022-03-09 09:20:31 +0100

    Fixed units in api url

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,7 +20,7 @@ export default function WeatherForecast(props) {
     let apiKey = "53315a4c01471ff10f1bbba4b3a95f94";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
